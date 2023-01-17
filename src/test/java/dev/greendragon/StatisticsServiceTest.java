b121372ad73f794ed449ae848a6f9587ffb4e960
Amy Chan <amy.chansya@gmail.com>
2023-01-17 14:45:46 -0500

    Add first two tests for StatisticsService unit test

---
+++ src/test/java/dev/greendragon/StatisticsServiceTest.java
@@ -0,0 +1,75 @@
+package dev.greendragon;
+
+import com.uni.daos.StatBasketballDAO;
+import com.uni.daos.UserDAO;
+import com.uni.dtos.PlayerCard;
+import com.uni.entities.ImUser;
+import com.uni.entities.StatBasketball;
+import com.uni.services.StatisticsService;
+import com.uni.services.StatisticsServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+public class StatisticsServiceTest {
+    private static  StatisticsServiceImpl statisticsService;
+    private static StatBasketballDAO statBasketballDAO;
+    private static UserDAO userDAO;
+
+    @BeforeEach
+    public void setup(){
+        statBasketballDAO = mock(StatBasketballDAO.class);
+        userDAO = mock(UserDAO.class);
+        statisticsService = new StatisticsServiceImpl(statBasketballDAO, userDAO);
+    }
+
+
+    @Test
+    public void getPlayerCardByUserId(){
+        when(userDAO.findById(anyInt())).thenReturn(new ImUser(1, "mockUser", "password", "player", 60, 180, "url", false));
+        PlayerCard playerCard = statisticsService.getPlayerCardByUserId(1);
+        verify(userDAO, times(1)).findById(1);
+        assertEquals("mockUser", playerCard.getUsername());
+    }
+
+    @Test
+    public void getAllBasketballStatsByGameId(){
+
+        // create mock stat list
+        List<StatBasketball> mockStatList = Stream.of(
+                new StatBasketball(1, 1, 1, "Mock Team 1", 1, 1, 1,1),
+                new StatBasketball(2, 2, 1, "Mock Team 2", 2, 2, 2,2)
+        ).collect(Collectors.toList());
+
+        // specify the mock behaviour
+        when(statBasketballDAO.findAllByGameId(anyInt())).thenReturn(mockStatList);
+
+        // calling the method in the service clas
+        List<StatBasketball> statList = statisticsService.getAllBasketballStatsByGameId(1);
+
+        //verifying if the method was invoked & if the result list is of correct size
+        verify(statBasketballDAO, times(1)).findAllByGameId(1);
+        assertEquals(2, statList.size());
+    }
+    @Test
+    public void addOrUpdateBasketballStat(){
+
+        // if stat exist, statBasketballDAO.update(existingStat) will be called
+        // if not exist, statBasketballDAO.save(stat) will be called
+
+    }
+
+
+}
