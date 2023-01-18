9f6ba4ac1ff79f820128e997ceff4dd6c8209a23
Amy Chan <amy.chansya@gmail.com>
2023-01-18 12:07:35 -0500

    Add third unit test(Work in progress) for  StatisticsService Test

--- src/test/java/dev/greendragon/StatisticsServiceTest.java
+++ src/test/java/dev/greendragon/StatisticsServiceTest.java
@@ -12,9 +12,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.exceptions.verification.TooManyActualInvocations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -63,13 +65,33 @@ public class StatisticsServiceTest {
         verify(statBasketballDAO, times(1)).findAllByGameId(1);
         assertEquals(2, statList.size());
     }
+
+    // Work in progress!
+    // still need to figure out how to test EITHER update() or save() is invoked
     @Test
     public void addOrUpdateBasketballStat(){
-
         // if stat exist, statBasketballDAO.update(existingStat) will be called
         // if not exist, statBasketballDAO.save(stat) will be called
 
+        List<StatBasketball> mockAllStatList = Stream.of(
+                new StatBasketball(1, 1, 1, "Mock Team 1", 1, 1, 1,1),
+                new StatBasketball(2, 2, 1, "Mock Team 2", 2, 2, 2,2),
+                new StatBasketball(3, 3, 2, "Mock Team 3", 3, 3, 3,3)
+        ).collect(Collectors.toList());
+
+        StatBasketball existingStat = new StatBasketball(3, 3, 2, "Mock Team 3", 10, 3, 3,10);
+
+        when(statBasketballDAO.findAll()).thenReturn(mockAllStatList);
+
+        StatBasketball resultStat = statisticsService.addOrUpdateBasketballStat(existingStat);
+
+
+        verify(statBasketballDAO).findAll();
+        verify(statBasketballDAO).update(any());
+        assertEquals(10, resultStat.getPoints());
+
+        // this would fail since save() is not called
+        // verify(statBasketballDAO).save((any()));
+
     }
-
-
 }
