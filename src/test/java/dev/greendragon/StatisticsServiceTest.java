c6e13c50a6e07d1b8f09d937b2681ff387ec99a8
Amy Chan <amy.chansya@gmail.com>
2023-01-19 12:06:16 -0500

    Edit StatisticService Unit tests

--- src/test/java/dev/greendragon/StatisticsServiceTest.java
+++ src/test/java/dev/greendragon/StatisticsServiceTest.java
@@ -30,6 +30,13 @@ public class StatisticsServiceTest {
     private static StatBasketballDAO statBasketballDAO;
     private static UserDAO userDAO;
 
+    List<StatBasketball> mockStatList = Stream.of(
+            new StatBasketball(1, 1, 1, "Mock Team 1", 1, 1, 1,1),
+            new StatBasketball(2, 2, 1, "Mock Team 2", 2, 2, 2,2),
+            new StatBasketball(3, 3, 1, "Mock Team 3", 3, 3, 3,3)
+
+    ).collect(Collectors.toList());
+
     @BeforeEach
     public void setup(){
         statBasketballDAO = mock(StatBasketballDAO.class);
@@ -40,20 +47,21 @@ public class StatisticsServiceTest {
 
     @Test
     public void getPlayerCardByUserId(){
-        when(userDAO.findById(anyInt())).thenReturn(new ImUser(1, "mockUser", "password", "player", 60, 180, "url", false));
+        ImUser mockUser = new ImUser(1, "mockUser", "password", "player", 60, 180, "url", false);
+        when(userDAO.findById(anyInt())).thenReturn(mockUser);
         PlayerCard playerCard = statisticsService.getPlayerCardByUserId(1);
         verify(userDAO, times(1)).findById(1);
         assertEquals("mockUser", playerCard.getUsername());
     }
 
+
+
+
     @Test
     public void getAllBasketballStatsByGameId(){
 
         // create mock stat list
-        List<StatBasketball> mockStatList = Stream.of(
-                new StatBasketball(1, 1, 1, "Mock Team 1", 1, 1, 1,1),
-                new StatBasketball(2, 2, 1, "Mock Team 2", 2, 2, 2,2)
-        ).collect(Collectors.toList());
+
 
         // specify the mock behaviour
         when(statBasketballDAO.findAllByGameId(anyInt())).thenReturn(mockStatList);
@@ -63,35 +71,56 @@ public class StatisticsServiceTest {
 
         //verifying if the method was invoked & if the result list is of correct size
         verify(statBasketballDAO, times(1)).findAllByGameId(1);
-        assertEquals(2, statList.size());
+        assertEquals(3, statList.size());
     }
 
     // Work in progress!
     // still need to figure out how to test EITHER update() or save() is invoked
     @Test
-    public void addOrUpdateBasketballStat(){
+    public void updateBasketballStat(){
         // if stat exist, statBasketballDAO.update(existingStat) will be called
+
+
+        StatBasketball statUpdate = new StatBasketball(3, 3, 1, "Mock Team 3", 99, 3, 3,999);
+
+        when(statBasketballDAO.findAll()).thenReturn(mockStatList);
+        StatBasketball updatedStat = statisticsService.addOrUpdateBasketballStat(statUpdate);
+
+        verify(statBasketballDAO, times(1)).findAll();
+        verify(statBasketballDAO,times(1)).update(any());
+        verify(statBasketballDAO,times(0)).save(any());
+
+        assertEquals(99, updatedStat.getPoints());
+        assertEquals(999, updatedStat.getFouls());
+
+
+    }
+
+    @Test
+    public void addBasketballStat(){
         // if not exist, statBasketballDAO.save(stat) will be called
 
-        List<StatBasketball> mockAllStatList = Stream.of(
-                new StatBasketball(1, 1, 1, "Mock Team 1", 1, 1, 1,1),
-                new StatBasketball(2, 2, 1, "Mock Team 2", 2, 2, 2,2),
-                new StatBasketball(3, 3, 2, "Mock Team 3", 3, 3, 3,3)
-        ).collect(Collectors.toList());
-
-        StatBasketball existingStat = new StatBasketball(3, 3, 2, "Mock Team 3", 10, 3, 3,10);
-
-        when(statBasketballDAO.findAll()).thenReturn(mockAllStatList);
-
-        StatBasketball resultStat = statisticsService.addOrUpdateBasketballStat(existingStat);
+        StatBasketball newStat = new StatBasketball();
+        newStat.setUserId(99);
+        newStat.setGameId(99);
+        newStat.setTeamName("NEW TEAM");
+        newStat.setPoints(0);
+        newStat.setRebounds(1);
+        newStat.setAssists(2);
+        newStat.setFouls(3);
 
 
-        verify(statBasketballDAO).findAll();
-        verify(statBasketballDAO).update(any());
-        assertEquals(10, resultStat.getPoints());
+        // throw NoSuchElementException to simulate no existing stat found
+        when(statBasketballDAO.findAll()).thenThrow(new NoSuchElementException());
+        StatBasketball newReturnedStat = statisticsService.addOrUpdateBasketballStat(newStat);
 
-        // this would fail since save() is not called
-        // verify(statBasketballDAO).save((any()));
+        // These two would pass
+        verify(statBasketballDAO, times(1)).findAll();
+        verify(statBasketballDAO,times(0)).update(any());
+        verify(statBasketballDAO, times(1)).save((any()));
+
+        // This fails because "newReturnedStat" is null
+        assertEquals("NEW TEAM", newReturnedStat.getTeamName());
 
     }
 }
